24ecdfd38935668e583b037023642f04c09bd80e
AAndersonSantos <andersonsantos.inc@gmail.com>
2025-08-07 17:13:14 -0300

    FEAT: Rotas login e dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/dashboard', fn () => view('dashboard.dashboard'));
